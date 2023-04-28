66fc8439406d110fdc3980f21ec922d39a21a861
Muneeb Ullah <58388634+moneebullah25@users.noreply.github.com>
2023-04-28 15:24:59 +0500

    Add files via upload

--- comment-remover.py
+++ comment-remover.py
@@ -1,23 +1,67 @@
+import tkinter as tk
+import tkinter.filedialog as filedialog
 import re
 
-def remove_comments(filepath):
-    with open(filepath, 'r') as f:
-        content = f.read()
+class App:
+    def __init__(self, master):
+        self.master = master
+        master.title("Comment Remover")
 
-    # Remove single-line comments
-    content = re.sub(r'#.*$', '', content, flags=re.MULTILINE)
+        # Create widgets
+        self.label1 = tk.Label(master, text="Select a Python file to remove comments from:")
+        self.button1 = tk.Button(master, text="Browse...", command=self.browse_file)
+        self.label2 = tk.Label(master, text="Remove redundant newlines:")
+        self.var1 = tk.BooleanVar()
+        self.var1.set(False)
+        self.radio1 = tk.Radiobutton(master, text="Yes", variable=self.var1, value=True)
+        self.radio2 = tk.Radiobutton(master, text="No", variable=self.var1, value=False)
+        self.button2 = tk.Button(master, text="Remove Comments", command=self.remove_comments)
+        self.status_label = tk.Label(master, text="")
 
-    # Remove multi-line comments enclosed in single quotes
-    content = re.sub(r"'''.*?'''.*?", '', content, flags=re.DOTALL)
+        # Lay out widgets
+        self.label1.grid(row=0, column=0, padx=10, pady=10)
+        self.button1.grid(row=0, column=1, padx=10, pady=10)
+        self.label2.grid(row=1, column=0, padx=10, pady=10)
+        self.radio1.grid(row=1, column=1, padx=10, pady=10)
+        self.radio2.grid(row=1, column=2, padx=10, pady=10)
+        self.button2.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+        self.status_label.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
 
-    # Remove multi-line comments enclosed in double quotes
-    content = re.sub(r'""".*?""".*?', '', content, flags=re.DOTALL)
+    def browse_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Python Files", "*.py")])
+        if file_path:
+            self.file_path = file_path
+            self.status_label.configure(text=f"Selected file: {file_path}")
 
-    # Write the modified content to the file
-    with open(filepath, 'w') as f:
-        f.write(content)
+    def remove_comments(self):
+        try:
+            # Read the contents of the file
+            with open(self.file_path, 'r') as f:
+                content = f.read()
 
-    print(f"Comments removed from {filepath}")
+            # Remove comments
+            content = re.sub(r'#.*$', '', content, flags=re.MULTILINE)
+            content = re.sub(r"'''.*?'''.*?", '', content, flags=re.DOTALL)
+            content = re.sub(r'""".*?""".*?', '', content, flags=re.DOTALL)
 
-# Example usage:
-remove_comments('activation.py')
+            # Remove redundant newlines if selected
+            if self.var1.get():
+                content = re.sub(r'\n{2,}', '\n', content)
+
+            # Write the modified contents to a new file
+            new_file_path = filedialog.asksaveasfilename(defaultextension=".py", filetypes=[("Python Files", "*.py")])
+            if new_file_path:
+                with open(new_file_path, 'w') as f:
+                    f.write(content)
+                self.status_label.configure(text=f"Comments removed from {self.file_path} and saved to {new_file_path}")
+            else:
+                self.status_label.configure(text="Save operation cancelled")
+        except AttributeError:
+            self.status_label.configure(text="No file selected")
+        except Exception as e:
+            self.status_label.configure(text=f"Error: {str(e)}")
+
+# Create and run the application
+root = tk.Tk()
+app = App(root)
+root.mainloop()
